c3d940e4320b06cc5277adaad18217ed1d97877e
lucasggmc <lucasggmc@gmail.com>
2021-07-15 22:26:20 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,43 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    console.log(newTaskTitle)
+    if(newTaskTitle !== ''){
+
+      const randomId = Math.floor(Math.random() * 10000);     
+
+      const newTask = {
+        id: randomId,
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      const newTasksList = [...tasks, newTask];
+      setTasks(newTasksList);
+      setNewTaskTitle('');
+
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let tasksChanged = tasks.map(task => {
+      if(task.id == id){
+        task.isComplete = !task.isComplete;
+      }
+
+      return task;
+    })
+    
+    setTasks(tasksChanged);
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    let arrayWithoutCurrentTask = tasks.filter(task => task.id !== id);
+    
+    setTasks(arrayWithoutCurrentTask);
   }
 
   return (
